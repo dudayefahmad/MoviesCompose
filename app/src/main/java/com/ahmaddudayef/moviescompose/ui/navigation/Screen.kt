627fc3cecf68c36a8e78cad4b430942410c83c75
Ahmad Dudayef <ahmaddudayef@gmail.com>
2024-11-25 11:02:33 +0700

    - add explore on bottom navigation menu on dashboard app

--- app/src/main/java/com/ahmaddudayef/moviescompose/ui/navigation/Screen.kt
+++ app/src/main/java/com/ahmaddudayef/moviescompose/ui/navigation/Screen.kt
@@ -1,9 +1,10 @@
 package com.ahmaddudayef.moviescompose.ui.navigation
 
 sealed class Screen(val route: String) {
-    object Movies : Screen("movies")
+    object Home : Screen("home")
     object DetailMovie : Screen("movie/{movieId}") {
         fun createRoute(movieId: Long) = "movie/$movieId"
     }
     object Profile : Screen("profile")
+    object Explore: Screen("explore")
 }

85fd4a3f753064a4aef279572b5990749214b3e9
Ahmad Dudayef <ahmaddudayef@gmail.com>
2024-11-27 14:45:20 +0700

    create unit test: - HomeViewModelTest.kt - DetailViewModelTest.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,4 +108,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutinesTest)
 }
